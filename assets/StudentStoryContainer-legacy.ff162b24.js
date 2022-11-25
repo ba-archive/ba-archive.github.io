f7fff9ceb1995b5ab19660551b6107a32130f2ac
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-11-26 00:26:37 +0900

    fix(momotalk): 学生头像会受到挤压的问题

--- assets/StudentStoryContainer-legacy.ff162b24.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy.2e3d3815.js"],(function(t,e){"use strict";var n,r,u,a;return{setters:[function(t){n=t.d,r=t.a,u=t.t,a=t.o}],execute:function(){var e={class:"flex-vertical rounded-small"};t("default",n({__name:"StudentStoryContainer",setup:function(t){return function(t,n){return a(),r("div",e," Student Story Container "+u(t.$route.params.id),1)}}}))}}}));
