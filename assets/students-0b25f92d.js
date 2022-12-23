7206cc26d918d2b8dc7d14c3fabb9aab7a427181
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-12-24 01:40:45 +0900

    feat(UX): fetch 失败时，向用户展示错误信息

--- assets/students-0b25f92d.js
+++
@@ -1,1 +0,0 @@
-import{y as t}from"./index-0c71c4a2.js";const s=t({id:"student",state:()=>({students:[]}),persist:!1,getters:{getAllStudents:t=>t.students,getStudentById:t=>s=>t.students.find((t=>t.id===s))},actions:{setStudents(t){this.students=t}}});export{s as u};
---
+++ assets/students-9a7d98c5.js
@@ -0,0 +1,1 @@
+import{z as t}from"./index-b6a76b7a.js";const s=t({id:"student",state:()=>({students:[]}),persist:!1,getters:{getAllStudents:t=>t.students,getStudentById:t=>s=>t.students.find((t=>t.id===s))},actions:{setStudents(t){this.students=t}}});export{s as u};
